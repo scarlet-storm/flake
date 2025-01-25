4b65fffa4f23fe3870c805b41eb0b69883206a91
scarletstorm <scarletstorm@noreply.codeberg.org>
2025-01-25 11:00:59 +0530

    enable mangohud

--- config/nixos/steam.nix
+++ config/nixos/steam.nix
@@ -8,35 +8,37 @@
     "steam-original"
     "steam-run"
   ];
-  programs.steam.enable = true;
-  programs.steam.remotePlay.openFirewall = true;
-  programs.steam.localNetworkGameTransfers.openFirewall = true;
-  hardware.steam-hardware.enable = true;
-  programs.steam.package = pkgs.steam.override {
-    buildFHSEnv = (
-      p:
-      pkgs.buildFHSEnvBubblewrap (
-        p
-        // {
-          PrivateTmp = true;
-          unshareCgroup = true;
-          unsharePid = true;
-          extraBwrapArgs = p.extraBwrapArgs ++ [
-            "--tmpfs /home"
-            "--bind \$HOME/.var/apps \$HOME"
-            "--ro-bind \$HOME/.config/dconf \$HOME/.config/dconf"
-            "--ro-bind \$HOME/.config/xsettingsd \$HOME/.config/xsettingsd"
-            # https://github.com/ValveSoftware/steam-for-linux/issues/10808 ???
-            "--ro-bind /run/current-system/sw/share/icons /usr/share/icons"
-            "--chdir \$HOME"
-          ];
-        }
-      )
-    );
-    extraEnv = {
-      MANGOHUD = true;
+  programs.steam = {
+    enable = true;
+    remotePlay.openFirewall = true;
+    localNetworkGameTransfers.openFirewall = true;
+    package = pkgs.steam.override {
+      buildFHSEnv = (
+        p:
+        pkgs.buildFHSEnvBubblewrap (
+          p
+          // {
+            PrivateTmp = true;
+            unshareCgroup = true;
+            unsharePid = true;
+            extraBwrapArgs = p.extraBwrapArgs ++ [
+              "--tmpfs /home"
+              "--bind \$HOME/.var/apps \$HOME"
+              "--ro-bind \$HOME/.config/dconf \$HOME/.config/dconf"
+              "--ro-bind \$HOME/.config/xsettingsd \$HOME/.config/xsettingsd"
+              # https://github.com/ValveSoftware/steam-for-linux/issues/10808 ???
+              "--ro-bind /run/current-system/sw/share/icons /usr/share/icons"
+              "--chdir \$HOME"
+            ];
+          }
+        )
+      );
+      extraEnv = {
+        MANGOHUD = true;
+      };
     };
+    extraPackages = with pkgs; [ mangohud ];
+    extraCompatPackages = [ pkgs.proton-ge-bin ];
   };
-  programs.steam.extraPackages = with pkgs; [ mangohud ];
-  environment.systemPackages = [ pkgs.bubblewrap ];
+  hardware.steam-hardware.enable = true;
 }
