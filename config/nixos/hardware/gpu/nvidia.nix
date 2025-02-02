574a319b0862346819de9f21580c6d5b5ae29036
scarlet-storm <12461256+scarlet-storm@users.noreply.github.com>
2025-02-02 10:11:21 +0530

    nvidia 570

--- config/nixos/hardware/gpu/nvidia.nix
+++ config/nixos/hardware/gpu/nvidia.nix
@@ -2,7 +2,6 @@
   config,
   lib,
   modules,
-  pkgs,
   ...
 }:
 
@@ -14,19 +13,7 @@
     powerManagement.enable = true;
     open = true;
     nvidiaSettings = true;
-    package = lib.mkDefault (
-      config.boot.kernelPackages.nvidiaPackages.beta
-      // {
-        open = config.boot.kernelPackages.nvidiaPackages.beta.open.overrideAttrs (prevAttrs: {
-          patches = prevAttrs.patches ++ [
-            (pkgs.fetchpatch {
-              url = "https://gitlab.archlinux.org/archlinux/packaging/packages/nvidia-utils/-/raw/2432b41b0946e035f133e733e604d41697a8afd5/silence-event-assert-until-570.patch";
-              hash = "sha256-0quYUU7mWDNgd81IzXlmVkch/zSPW9NxvIm4RlzEldA=";
-            })
-          ];
-        });
-      }
-    );
+    package = lib.mkDefault config.boot.kernelPackages.nvidiaPackages.beta;
   };
   unfree.packageList = [
     "nvidia-settings"
