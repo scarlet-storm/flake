7875fd83f7f8738e76947a904b1c18f39bc4fd9f
scarlet-storm <12461256+scarlet-storm@users.noreply.github.com>
2025-05-12 11:13:17 +0530

    Remove subuid & subgid files
    newuidmap doesn't work when /etc/subuid and /etc/subgid are symlinks

--- config/hosts/x86_64-linux/aqours/default.nix
+++ config/hosts/x86_64-linux/aqours/default.nix
@@ -79,10 +79,4 @@ in
   systemd.oomd.enable = true;
   systemd.oomd.enableRootSlice = true;
   boot.kernelParams = [ "split_lock_detect=off" ];
-  environment.etc = rec {
-    subuid.text = ''
-      violet:524288:65536
-    '';
-    subgid = subuid;
-  };
 }
