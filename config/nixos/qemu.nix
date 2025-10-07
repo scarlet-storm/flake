62987f0753074e7312a0de0eae1b3b5f97a7481c
scarlet-storm <12461256+scarlet-storm@users.noreply.github.com>
2025-10-07 12:13:13 +0530

    Remove libvirtd.qemu.ovmf deprecated option
    Drop libvirt verbatimConfig, not using nvidia or udmabuf (venus)

--- config/nixos/qemu.nix
+++ config/nixos/qemu.nix
@@ -7,20 +7,16 @@
       package = pkgs.qemu_kvm;
       runAsRoot = false;
       swtpm.enable = true;
-      ovmf = {
-        enable = true;
-        packages = [ pkgs.OVMFFull.fd ];
-      };
-      verbatimConfig = ''
-        namespaces = []
-        seccomp_sandbox = 0
-        cgroup_device_acl = [
-          "/dev/null", "/dev/full", "/dev/zero",
-          "/dev/random", "/dev/urandom",
-          "/dev/ptmx", "/dev/kvm", "/dev/udmabuf",
-          "/dev/nvidiactl", "/dev/nvidia0", "/dev/nvidia-modeset", "/dev/dri/renderD128", "/dev/dri/renderD129"
-        ]
-      '';
+      # verbatimConfig = ''
+      #   namespaces = []
+      #   seccomp_sandbox = 0
+      #   cgroup_device_acl = [
+      #     "/dev/null", "/dev/full", "/dev/zero",
+      #     "/dev/random", "/dev/urandom",
+      #     "/dev/ptmx", "/dev/kvm", "/dev/udmabuf",
+      #     "/dev/nvidiactl", "/dev/nvidia0", "/dev/nvidia-modeset", "/dev/dri/renderD128", "/dev/dri/renderD129"
+      #   ]
+      # '';
     };
   };
   systemd.services.libvirt-guests.unitConfig.After = [ "network-online.target" ];
