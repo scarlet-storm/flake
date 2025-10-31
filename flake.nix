f354e8c8f19378f1f339e2b13ed3f1d8d5698948
scarlet-storm <12461256+scarlet-storm@users.noreply.github.com>
2025-10-31 15:55:37 +0530

    unpin lanzaboote.inputs.rust-overlay
    nix-community/lanzaboote#485

--- flake.nix
+++ flake.nix
@@ -5,6 +5,10 @@
     nixpkgs = {
       url = "github:NixOS/nixpkgs/nixos-unstable";
     };
+    rust-overlay = {
+      url = "github:oxalica/rust-overlay";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
     home-manager = {
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
@@ -12,6 +16,7 @@
     lanzaboote = {
       url = "github:nix-community/lanzaboote/v0.4.2";
       inputs.nixpkgs.follows = "nixpkgs";
+      inputs.rust-overlay.follows = "rust-overlay";
     };
     sops-nix = {
       url = "github:Mic92/sops-nix";
