4cbc60cb547f099334e6d72e798bf3268e2b65a4
scarlet-storm <12461256+scarlet-storm@users.noreply.github.com>
2026-02-19 11:57:07 +0530

    nvidia painpeko

--- config/nixos/hardware/gpu/nvidia.nix
+++ config/nixos/hardware/gpu/nvidia.nix
@@ -2,10 +2,9 @@
 {
   services.xserver.videoDrivers = [ "nvidia" ];
   hardware.nvidia = {
-    modesetting.enable = true;
     powerManagement.enable = true;
     open = true;
     nvidiaSettings = true;
-    package = lib.mkDefault config.boot.kernelPackages.nvidiaPackages.beta;
+    package = lib.mkDefault config.boot.kernelPackages.nvidiaPackages.stable;
   };
 }
