e204b9e0e63312691fc5d918ac27a97bd43b142c
scarlet-storm <12461256+scarlet-storm@users.noreply.github.com>
2026-02-23 12:58:29 +0530

    ghostty: enable sudo and ssh-env features

--- config/home-manager/wm.nix
+++ config/home-manager/wm.nix
@@ -16,6 +16,7 @@ let
           command = "nu -i";
           scrollback-limit = 1 * 1024 * 1024 * 1024; # 1 GiB per pane
           window-inherit-working-directory = false;
+          shell-integration-features = "sudo,ssh-env";
         };
         package =
           if config.targets.genericLinux.enable then config.lib.nixGL.wrap pkgs.ghostty else pkgs.ghostty;
